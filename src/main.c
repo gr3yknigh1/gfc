884058f0bb2b90cc34e6e56c1448d382a9e84ebb
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-11-25 14:22:52 +0300

    Initialize Window

--- src/main.c
+++ src/main.c
@@ -1,6 +1,17 @@
+#include <raylib.h>
+
 #include "stdio.h"
 
 int main() {
-  printf("Hello world!\n");
+  InitWindow(1200, 900, "Game on Funcking C");
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    EndDrawing();
+  }
+
+  CloseWindow();
+
   return 0;
 }
